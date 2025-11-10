97311e1c3a89c2a28ea39386f92ed57e8491d600
Jaden <hyunbin.kim.3376@gmail.com>
2025-11-10 23:12:25 +0900

    [feat] 테스트

--- next.config.ts
+++ next.config.ts
@@ -12,8 +12,8 @@ const nextConfig: NextConfig = {
     ],
   },
   env: {
-    NEXT_PUBLIC_SITE_URL: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
+    NEXT_PUBLIC_SITE_URL: process.env.VERCEL_PROJECT_PRODUCTION_URL
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
       : 'http://localhost:3000',
   },
 };
